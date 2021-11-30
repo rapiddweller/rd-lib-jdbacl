c3387463a326beeab7b1c5586bf8119b577774e2
Volker Bergmann <volker.der.bergmann@gmx.de>
2021-11-30 19:32:37 +0100

    Migrated to new XML parser setup

---
+++ src/main/java/com/rapiddweller/jdbacl/JdbaclErrorIds.java
@@ -0,0 +1,21 @@
+/* (c) Copyright 2021 by Volker Bergmann. All rights reserved. */
+
+package com.rapiddweller.jdbacl;
+
+/**
+ * List errorIds used by jdbacl.<br/><br/>
+ * Created: 29.11.2021 13:27:55
+ * @author Volker Bergmann
+ * @since 1.1.12
+ */
+public class JdbaclErrorIds {
+  public static final String IDE_ILLEGAL_ATTRIBUTE = "IDE_0001";
+  public static final String IDE_TYPE = "IDE_0002";
+  public static final String IDE_TABLE = "IDE_0003";
+  public static final String IDE_NK_PK_QUERY = "IDE_0004";
+  public static final String IDE_SUB_NK_PK_QUERY = "IDE_0005";
+  public static final String IDE_PARENTS = "IDE_0006";
+  public static final String IDE_UNIQUE_KEY = "IDE_0007";
+  public static final String IDE_NATURAL_PK = "IDE_0008";
+  public static final String IDE_COLUMNS = "IDE_0009";
+}
